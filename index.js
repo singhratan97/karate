e48d8e0cc6e110c25fbc9d7724dcd8f11514384f
Ratan Singh <74782504+singhratan97@users.noreply.github.com>
2022-04-15 02:10:30 +0200

    new changes

---
+++ index.js
@@ -0,0 +1,20 @@
+const span = document.querySelector("span")
+
+let value = 0
+let count = 100
+
+
+let interval = setInterval(()=>{
+    if(value < count) {
+        value++
+        span.textContent = value
+        console.log(value)
+    }
+    else {
+        span.textContent = value
+        console.log("100 done")
+        clearInterval(interval)
+    }
+    console.log("final")
+},3)
+
