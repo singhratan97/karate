cda5033ce469c1961e6c78fa50c997b0ede390c6
Ratan Singh <74782504+singhratan97@users.noreply.github.com>
2022-05-14 21:59:30 +0530

    New changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const span = document.querySelector("span")
+
 const check = document.getElementById('check')
 
 check.addEventListener('click', function(){
@@ -15,7 +15,7 @@ document.getElementById("right-carousel").addEventListener("click", rightCarouse
 document.getElementById("left-carousel").addEventListener("click", leftCarousel)
 
 function rightCarousel() {
-        if(i === 2) {
+        if(i === 1) {
                 imgs[i].classList.add("img-carousel-hide");
                 i = 0;
                 imgs[i].classList.remove("img-carousel-hide")
@@ -30,7 +30,7 @@ function rightCarousel() {
 function leftCarousel() {
         if(i === 0) {
                 imgs[i].classList.add("img-carousel-hide");
-                i = 2;
+                i = 1;
                 imgs[i].classList.remove("img-carousel-hide")
         }
         else {
